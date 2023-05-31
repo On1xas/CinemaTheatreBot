ce42893de7abd44d0164e97c6012fa2a95e86aed
EVGENIY ALEYNIKOV <on1xas509@gmail.com>
2023-05-31 23:30:28 +0300

    Traning Inline Buttons

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,14 +1,26 @@
 from aiogram.filters import Command, CommandStart, Text
 from aiogram import Router
 from aiogram.types import Message
-from keyboards.keyboard_start import kb_start
+from keyboards.keyboard_start import BUTTONS, kb_start, kb_inl
 from keyboards.keyboards_afisha import kb_inl_afisha_main
+from aiogram.types import CallbackQuery
+from keyboards.keyboard_start import inl_button, create_inline_kb
+
+
+
+
 user_router: Router = Router()
 
 @user_router.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.answer(text='Я живой', reply_markup=kb_start)
+    keyboard = create_inline_kb(2, last_btn=None, b_1='1', b_2='2', b_3='3', b_4='4', b_5='5', b_6='Последняя кнопка')
+    await message.answer(text='Я живой', reply_markup=keyboard)
 
 @user_router.message(Command(commands=['afisha']))
 async def cmd_afisha(message: Message):
     await message.answer(text='Я живой', reply_markup=kb_inl_afisha_main)
+
+@user_router.callback_query(Text(text='TEST_PRESSED'))
+async def cb_test(callback: CallbackQuery):
+    await callback.message.answer(text='Нажали кнопку TEST')
+    await callback.answer(text="Нажат тест", show_alert=True)
--- keyboards/keyboard_start.py
+++ keyboards/keyboard_start.py
@@ -1,9 +1,58 @@
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
-from aiogram.types import Message
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
+from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 
 button_start=[KeyboardButton(text='Start')]
 
 kb_start_bl=ReplyKeyboardBuilder()
 kb_start_bl.row(*button_start)
 kb_start=kb_start_bl.as_markup(resize_keyboard=True, one_time_keyboard=True)
+
+inl_button=[[InlineKeyboardButton(text='TEST', callback_data='TEST_PRESSED')]]
+
+kb_inl=InlineKeyboardMarkup(inline_keyboard=inl_button)
+
+BUTTONS: dict[str, str] = {'btn_1': '1',
+                           'btn_2': '2',
+                           'btn_3': '3',
+                           'btn_4': '4',
+                           'btn_5': '5'}
+
+LEXICON: dict[str, str] = {'but_1': '1',
+                           'but_2': '2',
+                           'but_3': '3',
+                           'but_4': '4',
+                           'but_5': '5'}
+
+# Функция для генерации инлайн-клавиатур "на лету"
+def create_inline_kb(width: int,
+                     *args: str,
+                     last_btn: str | None = None,
+                     **kwargs: str) -> InlineKeyboardMarkup:
+    # Инициализируем билдер
+    kb_builder: InlineKeyboardBuilder = InlineKeyboardBuilder()
+    # Инициализируем список для кнопок
+    buttons: list[InlineKeyboardButton] = []
+
+    # Заполняем список кнопками из аргументов args и kwargs
+    if args:
+        for button in args:
+            buttons.append(InlineKeyboardButton(
+                text=LEXICON[button] if button in LEXICON else button,
+                callback_data=button))
+    if kwargs:
+        for button, text in kwargs.items():
+            buttons.append(InlineKeyboardButton(
+                text=text,
+                callback_data=button))
+
+    # Распаковываем список с кнопками в билдер методом row c параметром width
+    kb_builder.row(*buttons, width=width)
+    # Добавляем в билдер последнюю кнопку, если она передана в функцию
+    if last_btn:
+        kb_builder.row(InlineKeyboardButton(
+                            text=last_btn,
+                            callback_data='last_btn'))
+
+    # Возвращаем объект инлайн-клавиатуры
+    return kb_builder.as_markup()
